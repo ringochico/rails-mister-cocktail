22965dac9e92eca5f6dbc935154f775a6d0e75e8
Larry Nevins <ichbinlarry@gmail.com>
2018-08-09 17:47:27 +0900

    Styling started

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,31 @@
+class DosesController < ApplicationController
+
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+ def create
+   @cocktail = Cocktail.find(params[:cocktail_id])
+   @dose = Dose.new(dose_params)
+   @dose.cocktail = @cocktail
+   if @dose.save
+     redirect_to cocktail_path(@cocktail)
+   else
+     render :new
+   end
+  end
+
+  def destroy
+    @dose = Doses.find(params[:id])
+    @dose.destroy
+  end
+
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+end
