bf1aeaae80570f2fc783b07af60a9753be6606db
Larry Nevins <ichbinlarry@gmail.com>
2018-08-22 19:14:39 +0900

    Remodeled layout

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 function loadDynamicBannerText() {
   new Typed('#banner-typed-text', {
-    strings: ["There is always time for a cocktail"],
+    strings: ["There is always time for a cocktail", "Make one for any special occasion", "Don't know what to drink? Let's help you out!", "Welcome to Mister Cocktail", "Mix and drink"],
     typeSpeed: 50,
     loop: true
   });
